44e6a9fd9e32d23694b01e14c8600e5537241e09
Ryan Webster <ryan.h.webster@gmail.com>
2023-03-30 13:54:50 -0700

    migrations edited

--- db/migrations/20230330174715-create-tag.js
+++ db/migrations/20230330174715-create-tag.js
@@ -10,7 +10,9 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(20),
+        allowNull: false,
+        unique: true,
       },
       createdAt: {
         allowNull: false,
--- db/migrations/20230330175131-create-user.js
+++ db/migrations/20230330175131-create-user.js
@@ -10,16 +10,22 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(50),
+        unique: true,
+        allowNull: false,
       },
       firstName: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(30),
+        allowNull: false,
       },
       lastName: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(30),
+        allowNull: false,
       },
       hashedPassword: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING.BINARY,
+        allowNull: false,
+        unique: true,
       },
       createdAt: {
         allowNull: false,
