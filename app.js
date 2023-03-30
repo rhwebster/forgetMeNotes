1e8cc0ba0eb9f0b50db3f6f2415389d51d6f14b3
Ryan Webster <ryan.h.webster@gmail.com>
2023-03-30 14:10:14 -0700

    app

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+require('express-async-errors');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const { environment } = require('./config');
+
+const environment = require('./config');
+const { sequelize } = require('./db/models');
+const isProduction = environment === 'production';
+
+const app = express();
+app.set("view engine", "pug")
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(express.json());
+
+if (!isProduction) {
+    app.use(cors());
+}
+
+app.use(
+    helmet.crossOriginResourcePolicy({
+        policy: "cross-origin"
+    })
+);
+
+app.use(
+    csurf({
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && "Lax",
+            httpOnly: true
+        }
+    })
+)
+
+const store = new SequelizeStore({ db: sequelize });
