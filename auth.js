8fec3c68a5ad065696a7a5a8c5e6af8b69e76ff0
Ryan Webster <ryan.h.webster@gmail.com>
2023-04-05 11:24:06 -0400

    lists

--- auth.js
+++ auth.js
@@ -3,7 +3,7 @@ const db = require("./db/models");
 const loginUser = (req, res, user) => {
     req.session.auth = {
         userId: user.id,
-        firstName: user.firstName
+        userFirstName: user.firstName
     };
 };
 
