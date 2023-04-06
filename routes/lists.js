707313d42c3c27fcb61dc1a0d937f4a560bffb52
Ryan Webster <ryan.h.webster@gmail.com>
2023-04-06 10:04:31 -0400

    list routes

--- routes/lists.js
+++ routes/lists.js
@@ -89,3 +89,54 @@ router.get('/lists/tomorrow', requireAuth, csrfProtection, asyncHandler(async (r
         tomorrowList,
     });
 }));
+
+router.get('/lists/this-week', requireAuth, csrfProtection, asyncHandler(async (req, res) => {
+    const userId = req.session.auth.userId;
+    const weekList = await db.Task.findAll({
+        where: [
+            {
+                due: {
+                    [Op.gt]: new Date(),
+                    [Op.lt]: new Date(new Date().setDate(new Date().getDate()) + 7),
+                },
+            },
+            { userId },
+        ],
+    });
+    res.render("tasks-due-this-week", {
+        title: "Due This Week",
+        name: "This Week's Tasks",
+        path: "/lists/this-week",
+        csrfToken: req.csrfToken(),
+        weekList,
+    });
+}));
+
+router.post('/', requireAuth, csrfProtection, listValidator, asyncHandler(async (req, res) => {
+    const { name } = req.body;
+    const userId = req.session.auth.userId;
+    const list = db.List.build({
+        name,
+        userId,
+    });
+
+    const validatorErrors = validationResult(req);
+
+    if (validatorErrors.isEmpty()) {
+        await list.save();
+        res.redirect("/");
+    } else {
+        const errors = validatorErrors.array().map((error) => error.msg);
+        res.render("add-tag-or-list", {
+            title: "Add a List",
+            name: "List",
+            style: "./stylesheets/sign-up.css",
+            path: "/lists",
+            list,
+            errors,
+            csrfToken: req.csrfToken(),
+        });
+    }
+}));
+
+module.exports = router;
